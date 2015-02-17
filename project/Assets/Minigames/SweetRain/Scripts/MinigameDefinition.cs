ba41b246cdb26569a448254ac1dd3e1a4a022336
Raquel Jarillo <rachel.1991.15@gmail.com>
2015-02-17 02:56:21 +0100

    Creating minigame Sweet Rain.

---
+++ project/Assets/Minigames/SweetRain/Scripts/MinigameDefinition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace SweetRain
+{
+    public class MinigameDefinition : Minigame.Definition 
+    {
+            public MinigameDefinition()
+            {
+                SetInfo(
+                    "Sweet Rain",
+                    "SweetRain",
+                    4
+                    );
+             }
+     }
+}
---
+++ project/Assets/Minigames/SweetRain/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SweetRain
+{
+	public class PlayerController : Player.Controller
+	{
+        // Aqui van los métodos que implementan la logica del
+        // minijuego para el jugador.
+
+        protected override bool GameOverCheck()
+        {
+            return GetScore() >= 20;
+        }
+	}
+}

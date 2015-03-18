060bb9d810988e30e8f3cbf9d1ae029eb476c432
Raquel Jarillo <rachel.1991.15@gmail.com>
2015-03-18 12:32:05 +0100

    [Contar Objetos] Adding player to the  minigame. Step1

--- project/Assets/Minigames/ContarObjetos/Scripts/PlayerController.cs
+++ project/Assets/Minigames/ContarObjetos/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ namespace ContarObjetos
 {
     public class PlayerController : Player.Controller
     {
+
+        private float m_speed = 3;
+        private float m_gravity = 1;
+
         protected override void OnStart()
         {
             this.transform.FindChild("Name").GetComponent<TextMesh>().text = GetPlayer().GetNickname();
@@ -22,11 +26,27 @@ namespace ContarObjetos
 
         protected override void ProcessInput()
         {
+            Vector3 pos = this.transform.position;
+            Vector3 gravity = new Vector3(0, -m_gravity, 0);
+            //pos += gravity;
+
             this.transform.FindChild("Model").renderer.material.color = GetPlayer().GetPuppet().GetColor();
 
             if (GetPlayer().GetInput().IsKeyDown(Player.Input.Key.Action))
             {
                 this.transform.FindChild("Model").renderer.material.color = new Color(255, 0, 0);
+            }
+
+            if (GetPlayer().GetInput().IsKeyPressed(Player.Input.Key.Right))
+            {
+                Debug.Log("Right!");
+                this.transform.position = pos + new Vector3(m_speed * Time.deltaTime, 0, 0);
+            }
+
+            if (GetPlayer().GetInput().IsKeyPressed(Player.Input.Key.Left))
+            {
+                Debug.Log("Left!");
+                this.transform.position = pos - new Vector3(m_speed * Time.deltaTime, 0, 0);
             }
         }
 

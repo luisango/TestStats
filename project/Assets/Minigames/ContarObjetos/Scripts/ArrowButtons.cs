aae3149f65eacbc588172d3a5769373f9610976b
Laura <almonmanzano@gmail.com>
2015-04-20 22:45:02 +0200

    Arrows for counting objects added

---
+++ project/Assets/Minigames/ContarObjetos/Scripts/ArrowButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ArrowButtons : MonoBehaviour
+{
+    public Text text;
+
+    private int num = 0;
+
+    public void Update()
+    {
+        text.text = "" + num;
+    }
+
+    public void Add()
+    {
+        num++;
+    }
+
+    public void Sustract()
+    {
+        if (num >0) num--;
+    }
+}

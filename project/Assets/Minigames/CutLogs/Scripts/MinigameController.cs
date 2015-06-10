d451d22dd1dfe5f3507b7d01d8025c23b7a4f968
Raquel Jarillo <rachel.1991.15@gmail.com>
2015-06-11 01:13:19 +0200

    Modifying spawner characters. The The characters are arranged vertically around the cake.

--- project/Assets/Minigames/CutLogs/Scripts/MinigameController.cs
+++ project/Assets/Minigames/CutLogs/Scripts/MinigameController.cs
@@ -32,20 +32,26 @@ namespace CutLogs
         protected override void InstantiatePlayers()
         {
             // Hardcoded spawn metrics
-            Vector2 spawnRange = new Vector2(-11, 9);
+            Vector2 spawnRange = new Vector2(-5, 3);//new Vector2(-11, 9);
             float spawnLength = spawnRange.y - spawnRange.x;
-            float spawnStep = spawnLength / (Manager.Player.Instance.Get().Count * 2);
-            float lastSpawn = spawnRange.x;
+            //float spawnStep = spawnLength / (Manager.Player.Instance.Get().Count * 2);
+            //float lastSpawn = spawnRange.x;
+            float spawnStepX = spawnLength / (Manager.Player.Instance.Get().Count * 2);
+            float spawnStepY = spawnLength / (Manager.Player.Instance.Get().Count * 2);
+
+            float lastSpawnX = spawnRange.x;
+            float lastSpawnY = 0;
 
             for (int i = 0; i < Manager.Player.Instance.Get().Count; i++)
             {
-                lastSpawn += spawnStep;
-
                 // Instantiate player
                 GameObject newPlayer = InstantiatePlayer();
 
-                // Set positoin
-                newPlayer.transform.position = new Vector3(lastSpawn, 0, 0);
+                // Set position
+                //lastSpawn += spawnStep;
+                lastSpawnX += spawnStepX;
+                newPlayer.transform.position = new Vector3(lastSpawnX, lastSpawnY, 0); //new Vector3(lastSpawn, 0, 0);
+                lastSpawnY += spawnStepY;
 
                 // Add player as a child
                 newPlayer.transform.parent = this.transform;

63db2a177ecb60812f7d20a9e1dff5f6d86429f9
CarlosCasVen <carlos.castillo.venegas@gmail.com>
2015-07-03 07:46:53 +0200

    Victory scene + random roullete

---
+++ project/Assets/Scripts/GUI/Victory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Victory : MonoBehaviour
+{
+    public Text m_winner;
+
+
+    void Start()
+    {
+        // TODO: Shitty way to discover the winner... fix this shit!
+
+        Player.Wrapper winner = null;
+        int maxScore = 0;
+
+        foreach (Player.Wrapper player in Manager.Player.Instance.Get())
+        {
+            int thisPlayerScore = player.GetStats().GetPoints();
+
+            if (maxScore < thisPlayerScore || winner == null)
+            {
+                maxScore = thisPlayerScore;
+                winner = player;
+            }
+        }
+
+        m_winner.text = winner.GetNickname();
+    }
+
+    void Update()
+    {
+        foreach (Player.Wrapper player in Manager.Player.Instance.Get())
+        {
+            if (player.GetInput().IsKeyDown(Player.Input.Key.Action))
+            {
+                Manager.Scene.Instance.Load(Manager.Scene.Type.Credits);
+            }
+        }
+    }
+}

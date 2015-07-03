a7c4280cb4cec3dbc3044ab0cd84c82734ce8653
Luis Lalueza <luis.lalueza@gmail.com>
2015-07-03 07:47:01 +0200

    Premerge

---
+++ project/Assets/Scripts/GUI/Intro.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+	public void OnClickStartGame()
+    {
+        Manager.Scene.Instance.Load(Manager.Scene.Type.MainMenu);
+    }
+}
